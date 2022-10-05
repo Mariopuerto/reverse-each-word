a8c246dedc3334abbc3c5d9387b65cbc4aa39f9b
“mariopuerto” <mariopuerto.nyc@gmail.com>
2022-10-05 10:18:40 -0400

    commit tree 79163c83e401b6fef08f0fc4f2465787edf652f2

---
+++ reverse_each_word.rb
@@ -0,0 +1,16 @@
+
+    def reverse_each_word (sentence1)
+        array = sentence1.split
+        new_array = array.collect do |string|
+        string.reverse
+      end
+         new_array.join( " " )
+    end 
+
+
+#reverse_each_word
+# reverses all the words in a string without reversing the order of the words (FAILED - 1)
+# reverses all the words in another string without reversing the order of the words (FAILED - 2)
+# uses collect (FAILED - 3)
+
+
